417f2017d5ab1356c6842cef1b1dfbe41d348d41
Alexander Strakh <cromlehg@gmail.com>
2021-12-17 12:15:12 +0300

    Task 12 finished

---
+++ src/main/java/com/blockwit/leran1/cromlehg/Task12.java
@@ -0,0 +1,10 @@
+package com.blockwit.leran1.cromlehg;
+
+public class Task12 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world 1");
+        System.out.println("Hello world 2");
+    }
+
+}
